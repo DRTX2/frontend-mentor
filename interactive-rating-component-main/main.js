ece06518494a9170428a87326b9dbdf0d97cfff5
DRTX2 <davidmanjarres2004@gmail.com>
2025-05-01 19:47:12 -0500

    Add toggle efect with js

---
+++ interactive-rating-component-main/main.js
@@ -0,0 +1,21 @@
+const ratings=document.getElementById("ratings");
+const submitBtn=document.getElementById("send-rate");
+const howSection=document.querySelector(".how");
+const thanksSection=document.querySelector(".thanks");
+let lastSelectedElement;
+
+ratings.addEventListener("click",e=>{
+    if(!e.target.classList.contains("circle-item")) return;
+
+    if(lastSelectedElement) lastSelectedElement.classList.remove("selected");
+
+    lastSelectedElement=e.target;
+    lastSelectedElement.classList.add("selected");
+});
+
+submitBtn.addEventListener("click",e=>{
+    if(!lastSelectedElement) return;
+    
+    howSection.classList.toggle("active");
+    thanksSection.classList.toggle("active");
+});
